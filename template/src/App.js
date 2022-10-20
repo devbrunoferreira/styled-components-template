9b20dad989b1189f124a411acdb3c97d15c61b27
Bruno Ferreira <bgsf1991@gmail.com>
2022-10-20 00:37:51 -0300

    Template Style Components

--- template/src/App.js
+++ template/src/App.js
@@ -1,4 +1,5 @@
-import { Garagem } from "./Componentes/Garagem";
+import { Garagem }  from "./componentes/Garagem/Garagem";
+import { GlobalStyle } from "./GlobalStyle/GlobalStyle";
 
 export default function App() {
   const nome = "Labenu";
@@ -8,7 +9,8 @@ export default function App() {
   }
 
   return (
-    <div className="App">
+    <div>
+      <GlobalStyle/>
       <Garagem nome={nome} mensagemAprentacao={apresentaGaragem} />
     </div>
   );
